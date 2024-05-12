e092156c08d17d06e1df7b198b94cf95c9072824
john <36556216+shimspedy@users.noreply.github.com>
2024-05-12 03:53:20 -0700

    updated

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var gsRayExcel = './2023-general-schedule-pay-rates.xlsx';
-var leoRayExcel = './2023-law-enforcement-officer-pay-rates.xlsx';
+var gsRayExcel = './gs.xlsx';
+var leoRayExcel = './leo.xlsx';
